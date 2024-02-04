7979a0fdda81a455dce741f0c44fb3a064f1b8ce
Adityavyshnav <148896965+Adityavyshnav-2001@users.noreply.github.com>
2024-02-04 06:37:27 +0530

    modules import

---
+++ Shieldspeak_app1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pickle
+import pandas as pd
+import re
+#from flasgger import Swagger
+import streamlit as st 
+import modules as mod
+from PIL import Image
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from twilio.rest import Client
+
+import smtplib
+import ssl
+from email.message import EmailMessage
+import configparser
+
